a49237a1db563b31b3d17b6336cd6cb340eb6ca8
Lauricio Su <me@lauricio.com>
2017-04-10 10:43:47 +0700

    [npm] move to new package architecture

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,16 @@
 import { configure } from '@kadira/storybook';
 import { setOptions } from '@kadira/storybook-addon-options';
+import ThemeLoader
+  from '@ox2/theming/ThemeLoader';
+import { default as theme } from '@tapfuse/theme-spark';
+import '@ox2/css-font-roboto-condensed';
+import '@ox2/css-font-roboto';
+import '@ox2/css-icons-moon';
+import '@ox2/css-icons-material';
+import '@ox2/css-icons-ui';
+import '@ox2/ycss';
+import '@ox2/button-css';
+import '@ox2/storybook-css-layout';
 
 const req = require.context('../src/', true, /.story\.js$/);
 
@@ -12,5 +23,6 @@ setOptions({
   name: 'color-picker',
 });
 
+ThemeLoader({ theme });
 
 configure(loadStories, module);
--- src/ColorPickerChrome/ColorPickerChrome.js
+++ src/ColorPickerChrome/ColorPickerChrome.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { ChromePicker } from 'react-color';
 import Swatch from '../Swatch';
 
